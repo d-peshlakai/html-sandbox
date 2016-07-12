ee75759e481645934d12516fd4d0a2a65a3a86ad
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-12 15:08:34 -0600

    added comments

--- index.php
+++ index.php
@@ -5,20 +5,23 @@
 		<title>My Web Page</title>
 	</head>
 	<body>
+		<!--Begin Header-->
 		<header>
 			<h1>Hi world!</h1>
 		</header>
-
+		<!-- Start main content here -->
 		<main>
 		<p id="first-paragraph"><strong>Lorem Khaled Ipsum</strong> is a major key to <em>success</em>. Egg whites, turkey sausage, wheat toast, water. Of course they don’t want us to eat our breakfast, so we are going to enjoy our breakfast. Special cloth alert. In life you have to take the trash out, if you have trash in your life, take it out, throw it away, get rid of it, major key. You see the hedges, how I got it shaped up? It’s important to shape up your hedges, it’s like getting a haircut, stay fresh. To succeed you must believe. When you believe, you will succeed. Wraith talk. Cloth talk.
 		</p>
 		<h2 class="pretty-title red-text">About me</h2>
 		<p>Look at the sunset, life is amazing, life is beautiful, life is what you make it. The key to success is to keep your head above the water, never give up. They don’t want us to win. Eliptical talk. The ladies always say Khaled you smell good, I use no cologne. Cocoa butter is the key. In life there will be road blocks but we will over come it. Life is what you make it, so let’s make it. The key is to enjoy life, because they don’t want you to enjoy life. I promise you, they don’t want you to jetski, they don’t want you to smile.
 		</p>
+			<!--Unordered Lists-->
 		<ul>
 			<li>item 1</li>
 			<li>item 2</li>
 		</ul>
+			<!--Ordered Lists-->
 		<ol>
 		<li>item 1</li>
 		<li>item 2</li>
@@ -46,6 +49,7 @@
 			</tr>
 		</table>
 		</main>
+		<!--Table-->
 		<footer>
 			&copy; 2016
 		</footer>
